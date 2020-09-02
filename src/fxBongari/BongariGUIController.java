c26825c07cbbfc76e95acbb5fdfe4c68971d3fbb
jovimajo <itjonne@gmail.com>
2020-09-02 18:40:37 +0300

    Tehty alustava käyttöliittymä scenebuilderilla

--- src/fxBongari/BongariGUIController.java
+++ src/fxBongari/BongariGUIController.java
@@ -1,10 +1,75 @@
 package fxBongari;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import fi.jyu.mit.fxgui.Dialogs;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
 /**
  * @author Jonne
  * @version 2 Sep 2020
  *
  */
-public class BongariGUIController {
-	//
+public class BongariGUIController implements Initializable {
+
+    /**
+     * Käsitellään uuden jäsenen lisääminen
+     */
+    @FXML private void handleUusiJasen() {
+        Dialogs.showMessageDialog("Ei osata vielä lisätä");
+    }
+    
+    /**
+     * Käsitellään uuden bongauksen lisääminen
+     */
+    @FXML private void handleUusiBongaus() {
+        Dialogs.showMessageDialog("Ei osata vielä lisätä");
+    }
+    
+    /**
+     * Käsitellään bongauksen poistaminen
+     */
+    @FXML private void handlePoistaBongaus() {
+        Dialogs.showMessageDialog("Ei osata vielä lisätä");
+    }
+    
+    /**
+     * Käsitellään tallennuskäsky
+     */
+    @FXML private void handleTallenna() {
+        tallenna();
+    }
+    
+    /**
+     * Käsitellään lopetuskäsky
+     */
+    @FXML private void handleLopeta() {
+        tallenna();
+        Platform.exit();
+    }
+
+    /**
+     * Tarkistetaan onko tallennus tehty
+     * @return true jos saa sulkea sovelluksen, muuten false
+     */
+    public boolean voikoSulkea() {
+        tallenna();
+        return true;
+    }
+    
+    /**
+     * Tietojen tallennus
+     */
+    private void tallenna() {
+        Dialogs.showMessageDialog("Tallennetetaan! Mutta ei toimi vielä");
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+        
+    }
 }
--- src/fxBongari/BongariMain.java
+++ src/fxBongari/BongariMain.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -16,10 +17,20 @@ public class BongariMain extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("BongariGUIView.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+		    final FXMLLoader ldr = new FXMLLoader(getClass().getResource("BongariGUIView.fxml"));
+            final Pane root = (Pane)ldr.load();
+            final BongariGUIController bongariCtrl = (BongariGUIController)ldr.getController();
+            
+            final Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("bongari.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Bongari");
+            
+            primaryStage.setOnCloseRequest((event) -> {
+                // Kutsutaan voikoSulkea-metodia
+                if ( !bongariCtrl.voikoSulkea() ) event.consume();
+            });
+            
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
